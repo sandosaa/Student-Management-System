acd3b4c2fe66251ad4af3ad855f5be1fe4a650d8
Sondos Mohamed <sondos_ali_2006@hotmail.com>
2025-12-09 22:34:46 +0200

    Finished first case (add new stedent) & linked to csv file

--- StudentMS.cpp
+++ StudentMS.cpp
@@ -1,24 +1,54 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
 class Student{
 private:
-    string name;
-    int ID;
-    float gpa;
-    string department;
-    int level;
+    string name="Not signed";
+    int ID=0;
+    float gpa=0;
+    string department="Not signed";
+    int level=0;
     string email="Not signed"; //optional 
 public:
-    Student(string name="Not signed",int ID=0,float gpa=0,
-         string department="Not signed",int level=0){
-        this -> name=name;
-        this -> ID=ID;
-        this -> gpa=gpa;
-        this -> department=department;
-        this -> level=level;
-    };
+    // Student(string name="Not signed",int ID=0,float gpa=0,
+    //      string department="Not signed",int level=0){
+    //     this -> ID=ID;
+    //     this -> name=name;
+    //     this -> gpa=gpa;
+    //     this -> department=department;
+    //     this -> level=level;
+    // };
+
+    void set_name(string name){
+        this -> name = name;
+    }
+
+    void set_ID(int ID){
+        this -> ID = ID;
+    }
+
+    void set_gpa(int gpa){
+        this -> gpa = gpa;
+    }
+
+    void set_department(string department){
+        this -> department = department;
+    }
+
+    void set_level(int level){
+        this -> level = level;
+    }
+
+    void set_email(string email){
+        if(email.find('@'))
+        this -> email=email;
+        else{
+            cout<<"This email is not valid";
+        }
+    }
+
     void display(){
         cout<<"++++++++++++++++++++++++++++"<<endl;
         cout<<"Name: "<<name<<endl;
@@ -31,6 +61,19 @@ public:
         cout<<"++++++++++++++++++++++++++++"<<endl;
 
     }
+    void add_student_to_CSV(int id, string name, float gpa,
+         string department, int level,string email){
+            ofstream file("student_data.csv",ios::app);
+            if(file.is_open()){
+            file<<id<<','<<name<<","<<gpa<<","<<department<<","<<level<<","<<email;
+            cout<<"Your data added in student_data.csv file!";
+            file.close();}
+            else{
+                cout<<"Can't add the student to the file"<<endl;
+            }
+
+
+    }
 
 };
 
@@ -38,7 +81,6 @@ public:
 
 int main(){
     while(true){
-        try_again:
         cout<<"*************************************"<<endl;
         cout<<"***** Student Management System *****"<<endl;
         cout<<"*************************************"<<endl;
@@ -60,6 +102,52 @@ int main(){
         switch (choice)
         {
         case 1:
+        {Student s1;
+
+        string name;
+        cout<<"Enter your name: ";
+        cin>>name;
+        s1.set_name(name);
+
+        int ID;
+        cout<<"Enter your ID: ";
+        cin>>ID;
+        cout<<endl;
+        s1.set_ID(ID);
+
+        float gpa;
+        cout<<"Enter your GPA: ";
+        cin>>gpa;
+        cout<<endl;
+        s1.set_gpa(gpa);
+
+        string dep;
+        cout<<"Enter your department: ";
+        cin>>dep;
+        s1.set_department(dep);
+
+        int level;
+        cout<<"Enter your level: ";
+        cin>>level;
+        cout<<endl;
+        s1.set_level(level);
+
+        string add_email_or_no;
+        string email="Not signed";
+        cout<<"Do you want to add email?(y/n) : ";
+        cin>>add_email_or_no;
+        if (add_email_or_no=="y" or add_email_or_no=="Y"){
+            string email;
+            cout<<"\nEnter your Email: ";
+            cin>>email;
+            s1.set_email(email);
+        }
+        
+        s1.add_student_to_CSV(ID,name,gpa,dep,level,email);
+        
+        
+        }
+
             break;
         case 2:
             break;
@@ -70,13 +158,14 @@ int main(){
         case 5:
             break;
         case 6:
-            break;
+            cout<<"Thank you for using Pika team program <3"<<endl;
+            exit(0);
         default: //the user entered wrong #
             cout<<"Invalid #, try again! "<<endl;
-            goto try_again;
         }
     
     }
 
     return 0;
 }
+
